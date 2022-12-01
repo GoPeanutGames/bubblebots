e77f03af2d5881ff6be301cf44a3d4165a6036cc
Machrom <contact.mscreations@gmail.com>
2022-12-01 18:26:37 +0100

    setting up buttons and menu to work

--- Assets/Scripts/GameScreens/Home/HomeScreenController.cs
+++ Assets/Scripts/GameScreens/Home/HomeScreenController.cs
@@ -2,8 +2,18 @@ using UnityEngine;
 
 public class HomeScreenController : MonoBehaviour
 {
+    public PlayButtonController PlayButtonController;
+    public GameObject UpperInfoContainer;
+    public GameObject BottomMenuContainer;
     public GameObject HomeScreen;
     public GameObject ModeSelectScreen;
+    public GameObject StoreScreen;
+
+    private void SetInfoContainers(bool upperActive, bool bottomActive)
+    {
+        UpperInfoContainer.SetActive(upperActive);
+        BottomMenuContainer.SetActive(bottomActive);
+    }
 
     private void ResetAllScreens()
     {
@@ -11,15 +21,26 @@ public class HomeScreenController : MonoBehaviour
         ModeSelectScreen.SetActive(false);
     }
 
-    public void PlayButtonClick()
+    public void OpenModeSelectScreen()
     {
         ResetAllScreens();
+        SetInfoContainers(true, false);
         ModeSelectScreen.SetActive(true);
+    }
+
+    public void OpenStoreScreen()
+    {
+        ResetAllScreens();
+        SetInfoContainers(true, true);
+        PlayButtonController.SetPlayButtonState(false);
+        StoreScreen.SetActive(true);
     }
 
     public void BackToHome()
     {
         ResetAllScreens();
+        SetInfoContainers(true, true);
+        PlayButtonController.SetPlayButtonState(true);
         HomeScreen.SetActive(true);
     }
 }
