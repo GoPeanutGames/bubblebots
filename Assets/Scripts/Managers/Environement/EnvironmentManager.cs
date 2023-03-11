e3c7b82d3b8020744f9852d318b9ca7f3739f21c
Marko <contact.mscreations@gmail.com>
2023-03-11 09:26:15 +0100

    task(minibots): config for production (apple)

--- Assets/Scripts/Managers/Environement/EnvironmentManager.cs
+++ Assets/Scripts/Managers/Environement/EnvironmentManager.cs
@@ -3,34 +3,6 @@ using UnityEngine;
 
 public class EnvironmentManager : MonoSingleton<EnvironmentManager>
 {
-    /*TODO: config for:
-     ========DEV
-     BundleId: com.peanutgames.minibotsdevnew
-     LastBundleVersion: 31s
-     GOOGLE PLAY GAMES (I used web for both?):
-     APP ID: 916173128763
-     GPG Setup ID (android): 916173128763-0b6c97bkn7ck2120nlsuro5k0vvne1fn.apps.googleusercontent.com
-     GPG Client ID: 916173128763-tqei703s3fab014kl49defmbnl73k88n.apps.googleusercontent.com
-     GPG Client secret: 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
-     APPLE:
-     APP ID: 1670620983
-     REVENUE CAT:
-     Api key Google: goog_jPpsekNkdZvSzPwJcWLkSvLRFof
-     Api key Apple: appl_sHJjOuUTDbFSdiXJMPAPiAiMnTY
-     =========PROD
-     BundleId: com.peanutgames.minibots.prod
-     LastBundleversio: 2
-     GOOGLE PLAY GAMES: 
-     GPG Setup ID (android): 289220171644-gqbpa74u8g63hs3fh5cuj20c9qf0rjtp.apps.googleusercontent.com 
-     GPG Client ID: 289220171644-b5e3j0mdhhc6ov1beb46nh8qjel9ils1.apps.googleusercontent.com
-     GPG Client secret: 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
-     APPLE: 
-     APP ID: 289220171644
-     REVENUE CAT:
-     Api key Google: goog_MXLQDoEZTrocHYukgyiRfmiCfaX
-     Api key Apple:
-     */
-    
     [SerializeField] private bool Development;
     [SerializeField] private bool Production;
     [SerializeField] private bool Rhym;
