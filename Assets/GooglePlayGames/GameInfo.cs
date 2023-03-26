25f6b1419c0404091faf902f503dd6f3a2ad0622
Marko <contact.mscreations@gmail.com>
2023-03-26 18:23:51 +0200

    prod config for apple

--- Assets/GooglePlayGames/GameInfo.cs
+++ Assets/GooglePlayGames/GameInfo.cs
@@ -36,9 +36,9 @@ namespace GooglePlayGames {
         private const string UnescapedWebClientId = "WEB_CLIENTID";
         private const string UnescapedNearbyServiceId = "NEARBY_SERVICE_ID";
 
-        public const string ApplicationId = "916173128763"; // Filled in automatically
+        public const string ApplicationId = "289220171644"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
-        public const string WebClientId = "916173128763-tqei703s3fab014kl49defmbnl73k88n.apps.googleusercontent.com"; // Filled in automatically
+        public const string WebClientId = "289220171644-b5e3j0mdhhc6ov1beb46nh8qjel9ils1.apps.googleusercontent.com"; // Filled in automatically
         public const string NearbyConnectionServiceId = "";
 
         public static bool ApplicationIdInitialized() {
