438e3c450ce39903e30aac050a60a4ff24e1c8b8
ccir105 <sisnet2010@gmail.com>
2024-03-08 16:59:08 +0545

    Update init.js with purchasing of gems in blast network (#65)

--- Assets/WebGLTemplates/BubbleBotsTemplate/TemplateData/init.js
+++ Assets/WebGLTemplates/BubbleBotsTemplate/TemplateData/init.js
@@ -1,164 +1,58 @@
 const contractAbi = [
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "uint256",
-        name: "bundleId",
-        type: "uint256",
+        "internalType": "uint256",
+        "name": "bundleId",
+        "type": "uint256"
       },
+      {
+        "internalType": "uint256",
+        "name": "amountInEth",
+        "type": "uint256"
+      },
+      {
+        "internalType": "bytes",
+        "name": "_signature",
+        "type": "bytes"
+      }
     ],
-    name: "purchaseGemsByToken",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    constant: false,
-    inputs: [
-      {
-        name: "guy",
-        type: "address",
-      },
-      {
-        name: "wad",
-        type: "uint256",
-      },
-    ],
-    name: "approve",
-    outputs: [
-      {
-        name: "",
-        type: "bool",
-      },
-    ],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [
-      {
-        name: "",
-        type: "address",
-      },
-      {
-        name: "",
-        type: "address",
-      },
-    ],
-    name: "allowance",
-    outputs: [
-      {
-        name: "",
-        type: "uint256",
-      },
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "uint256",
-        name: "bundleId",
-        type: "uint256",
-      },
-    ],
-    name: "getBundle",
-    outputs: [
-      {
-        components: [
-          {
-            internalType: "uint256",
-            name: "price",
-            type: "uint256",
-          },
-          {
-            internalType: "uint256",
-            name: "beforeDiscount",
-            type: "uint256",
-          },
-          {
-            internalType: "uint256",
-            name: "gems",
-            type: "uint256",
-          },
-          {
-            internalType: "bool",
-            name: "isActive",
-            type: "bool",
-          },
-          {
-            internalType: "string",
-            name: "name",
-            type: "string",
-          },
-          {
-            internalType: "string",
-            name: "description",
-            type: "string",
-          },
-        ],
-        internalType: "struct Bubbles.Bundle",
-        name: "",
-        type: "tuple",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [
-      {
-        name: "",
-        type: "address",
-      },
-    ],
-    name: "balanceOf",
-    outputs: [
-      {
-        name: "",
-        type: "uint256",
-      },
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
+    "name": "purchaseGemsByEth",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
+  }
 ];
 
 const env = {
   dev: {
-    chainId: 80001,
-    gemsContract: "0xB37F565A4205B6A0dA009262e1d9B3b9d494E166",
+    chainId: 168587773,
+    gemsContract: "0x7F119F5f554B5a13f806F708aA74855523773828",
     tokenContract: "0xeef2D7cA3d6fD039dF5Eb899378a37F541bDefA0",
     connectionConfig: {
-      rpcUrls: ["https://rpc-mumbai.maticvigil.com"],
-      chainName: "Matic Mumbai",
+      rpcUrls: ["https://sepolia.blast.io"],
+      chainName: "Blast Sepolia",
       nativeCurrency: {
-        name: "MATIC",
-        symbol: "MATIC",
+        name: 'ETH',
+        symbol: 'ETH',
         decimals: 18,
       },
-      blockExplorerUrls: ["https://mumbai.polygonscan.com/"],
+      blockExplorerUrls: ["https://testnet.blastscan.io"],
     },
   },
   prod: {
-    chainId: 137,
-    gemsContract: "0xcA0ccb920267C78259CF333959F48D649Be13E34",
+    chainId: 81457,
+    gemsContract: "0xf1bed3036BC5C1A16cAb7BdAa9234ee4e49CB57F",
     tokenContract: "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
     connectionConfig: {
-      rpcUrls: ["https://polygon-rpc.com"],
-      chainName: "Polygon Mainnet",
+      rpcUrls: ["https://blastscan.io"],
+      chainName: "Blast Mainnet",
       nativeCurrency: {
-        name: "MATIC",
-        symbol: "MATIC",
+        name: 'ETH',
+        symbol: 'ETH',
         decimals: 18,
       },
-      blockExplorerUrls: ["https://polygonscan.com/"],
+      blockExplorerUrls: ["https://blastscan.io"],
     },
   },
 };
@@ -222,54 +116,47 @@ window.metamaskBundleBuying = async (bundleId = 1, isDev = true) => {
       contractAbi,
       provider
     );
-    const usdcContract = new ethers.ethers.Contract(
-      currentEnv.tokenContract,
-      contractAbi,
-      provider
-    );
 
     let userAddress = await ethereum.request({ method: "eth_requestAccounts" });
     userAddress = userAddress[0];
 
-    //get the bundle details from contract
-    const bundleDetails = await gemsContract.getBundle(bundleId);
+    const baseUrl = isDev === true ? `https://api-bb.peanuthub.dev/` : `https://api-bb.peanutgames.com/`;
 
-    const bundlePrice = bundleDetails.price.toNumber();
+    //get the bundle purchasing request
+    const buyRequest = await fetch(`${baseUrl}bundles/prepare-gems-purchase-with-eth'`, {
+      method: 'POST',
+      body: JSON.stringify({ address: userAddress.toLowerCase(), bundleId: `${bundleId}` }),
+    })
 
-    //get the token allowance
-    const tokenAllowance = await usdcContract.allowance(
-      userAddress,
-      currentEnv.gemsContract
-    );
-
-    //if not approved, approve the balance, max number so next time user won't have to approve again
-    if (tokenAllowance < bundlePrice) {
-      let MAX_INT = 11579208923731619542357098500868790785;
-
-      const tx = await usdcContract
-        .connect(provider.getSigner())
-        .approve(currentEnv.gemsContract, BigInt(MAX_INT));
-
-      await tx.wait();
+    if (!buyRequest) {
+      console.log("You are not eligible to buy this bundle. Please try again later.");
+    
+      return false
     }
 
-    //get the token balance of user
-    let userBalance = await usdcContract.balanceOf(userAddress);
+    const userBalance = await provider.getBalance(
+      userAddress
+    )
 
-    userBalance = userBalance.toNumber();
-
-    //if he has enough balance purchase the gems here
-    if (userBalance >= bundlePrice) {
-      const tx = await gemsContract
-        .connect(provider.getSigner())
-        .purchaseGemsByToken(bundleId);
-
-      await tx.wait();
-
-      return true;
-    } else {
-      console.log("Not Enough balance to purchase");
+    if (userBalance.lt(ethers.BigNumber.from(buyRequest.priceInWei))) {
+      console.log(
+        `You don't have enough ETH to buy this bundle. Please try again later.`,
+      )
+      return false
     }
+
+    const tx = await gemsContract
+      .connect(provider.getSigner())
+      .purchaseGemsByEth(
+        bundleId,
+        buyRequest.priceInWei,
+        buyRequest.signature,
+        {
+          value: buyRequest.priceInWei,
+        }
+      )
+
+      await tx.wait()
   }
 };
 
