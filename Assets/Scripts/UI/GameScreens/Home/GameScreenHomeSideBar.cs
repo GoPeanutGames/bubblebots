4aa36b33ed29ff9688ec20800af3a3023bde22fd
Marius Gherman <mariusgherman8@gmail.com>
2024-03-25 10:59:11 +0200

    all sidebar texts are the same size

--- Assets/Scripts/UI/GameScreens/Home/GameScreenHomeSideBar.cs
+++ Assets/Scripts/UI/GameScreens/Home/GameScreenHomeSideBar.cs
@@ -1,3 +1,32 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+
 public class GameScreenHomeSideBar : GameScreenAnimatedShowHide
 {
+    public List<TMPro.TextMeshProUGUI> buttonsTexts;
+
+
+    public async Task ApplySameSizeTexts()
+    {
+        await Task.Delay(100);
+        buttonsTexts.ForEach(x => x.ForceMeshUpdate());
+
+
+        float minSize = buttonsTexts.Select(x => x.fontSize).Min();
+        buttonsTexts.ForEach(x =>
+        {
+            x.enableAutoSizing = false;
+            x.fontSize = minSize;
+        });
+    }
+
+    override protected void Awake()
+    {
+        base.Awake();
+        _ = ApplySameSizeTexts();
+    }
+
+
 }
