d7f97bb1309f579e34147f89d9df23410e29dd99
Vitaly Tikhoplav <62797411+tikhoplav@users.noreply.github.com>
2024-04-18 18:53:42 +0300

    Added signalling with parent document
    This allows client document to receive auth info from parent document, when mounted as an `iframe`.

--- Assets/WebGLTemplates/BubbleBotsTemplate/TemplateData/init.js
+++ Assets/WebGLTemplates/BubbleBotsTemplate/TemplateData/init.js
@@ -223,3 +223,31 @@ document.addEventListener("readystatechange", () => {
     });
   };
 });
+
+/**
+ * When game is mounted in an `iframe` the code below
+ * allows to receive session data from the parent window.
+ */
+window.addEventListener('message', event => {
+  const { signal, data } = event.data
+  switch (signal) {
+    case "auth":
+      return saveAuthInfo(data)
+  }
+})
+
+const saveAuthInfo = data => {
+  localStorage.setItem('web3Info', JSON.stringify(data))
+}
+
+window.getAuthInfo = () => {
+  const data = localStorage.getItem('web3info')
+  if (!data) {
+    throw new Error('no auth data saved')
+  }
+  return data
+}
+
+window.clearAuthInfo = () => {
+  localStorage.delete('web3info')
+}
